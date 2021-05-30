8aeab3eecee5c2785ef57c837093862f902e1f92
JustinLouazel <81767477+JustinLouazel@users.noreply.github.com>
2021-05-30 19:42:05 +0200

    Add files via upload

--- player.py
+++ player.py
@@ -7,6 +7,11 @@
 
 import numpy as np
 import pandas as pd
+import pulp
+
+from bokeh.layouts import row
+from bokeh.plotting import figure, show
+
 
 data_csv=pd.read_csv("/home/justin/Documents/Cours/Optimisation_et_energie/seriousGame/microgrid-player/data/pv_prod_scenarios.csv",delimiter=";")
 
@@ -31,11 +36,16 @@ class Player:
 		self.battery = Battery(self.horizon)
 		self.pMax = 10#kWh
 
+		self.pd = 0.95 # discharge efficiency
+		self.pc = 0.95 # charge efficiency
+		self.C = 30#kWh -- capacity of the battery
+
+
+
 	def set_scenario(self, scenario_data):
 		data_selected = scenario_data[scenario_data["region"]==self.region_name][scenario_data[scenario_data["region"]==self.region_name]["day"]==self.day] # on selectionne les données sur la région et le jour considéré.
 		self.data = np.zeros(self.horizon)
 		for i in range(0, 24): # on remplit deux cases à chaque fois avec la même production car les données sont données par heures
-			print(i)
 			self.data[2*i]=data_selected["pv_prod (W/m2)"][i]
 			self.data[2*i+1]=data_selected["pv_prod (W/m2)"][i]
 
@@ -45,31 +55,107 @@ class Player:
 		self.prices = prices
 
 	def compute_all_load(self):
-		load = np.zeros(self.horizon)
-		for time in range(self.horizon):
-			load[time] = self.compute_load(time)
-			print(self.battery.getBattery(time))
 
-		return load
+		lp = pulp.LpProblem("lp", pulp.LpMinimize)
+		lp.setSolver()
+		l_bat = {}
+
+		l_bat_plus = {}
+		l_bat_moins = {}
+
+		for t in range(self.horizon):
+			var_name = "l_bat n°"+str(t)
+			l_bat[t] = pulp.LpVariable(var_name,-self.pMax,self.pMax) # la mise dans la batterie est limitée par pMax
+			constraint_name = "power_limit_l_bat n°"+str(t)
+			lp += l_bat[t]-self.data[t]<=self.pMax,constraint_name
+
+			var_name = "l_bat_plus n°"+str(t)
+			l_bat_plus[t] = pulp.LpVariable(var_name,0, None)
+			constraint_name = "upper_bound_l_bat_plus" + str(t)
+			lp+= l_bat[t]<=l_bat_plus[t],constraint_name
+
+			var_name = "l_bat_moins n°"+str(t)
+			l_bat_moins[t] = pulp.LpVariable(var_name,0, None)
+			constraint_name = "upper_bound_l_bat_moins" + str(t)
+			lp+= -l_bat[t]<=l_bat_moins[t],constraint_name
+
+			constraint_name = "equality of battery exchanges"+str(t)
+			lp+= l_bat[t] == l_bat_plus[t]-l_bat_moins[t],constraint_name
+
+			constraint_name = "lower_bound_stock_"+str(t)
+			lp += self.pc*pulp.lpSum([l_bat_plus[i] for i in range(t)])-1/self.pd*pulp.lpSum([l_bat_moins[i] for i in range(t)]) >= 0,constraint_name
+			constraint_name = "upper_bound_stock_"+str(t)
+			lp += (self.pc*pulp.lpSum([l_bat_plus[i] for i in range(t)])-1/self.pd*pulp.lpSum([l_bat_moins[i] for i in range(t)])) <= self.C/self.time_interval,constraint_name
+
+			constraint_name = "dispo_batterie" + str(t)
+			lp+= l_bat[t]<=self.data[t], constraint_name # l'intant initial, la batterie est vide, on peut simplement remplir ce qui vient d'être produit
+
+
+
+		constraint_name = "empty the battery"
+		lp += (self.pc*pulp.lpSum([l_bat_plus[i] for i in range(self.horizon)])-1/self.pd*pulp.lpSum([l_bat_moins[i] for i in range(self.horizon)]))*self.time_interval == 0,constraint_name
+
+
+
+		lp.setObjective(pulp.lpSum([self.prices[t]*(l_bat[t]-self.data[t]) for t in range(self.horizon)]))
+		lp.solve()
+
+		affLbat = np.zeros(self.horizon)
+		affBatt = np.zeros(self.horizon)
+		for t in range(self.horizon):
+			affLbat[t]=l_bat[t].varValue
+			if t!= 0 :
+				affBatt[t]=affBatt[t-1]+(self.pc*l_bat_plus[t].varValue-1/self.pd*l_bat_moins[t].varValue)*self.time_interval
+
+
+##Affichage des résultats
+		p_prix = figure(title="Prix par jour", x_axis_label="t", y_axis_label="prix")
+		p_prix.vbar(x=range(self.horizon),top=self.prices)
+
+
+		p_prod = figure(title="Productions par jour",x_axis_label="pas de temps t",y_axis_label="Production en kW")
+		p_prod.line(range(self.horizon),affLbat,legend_label="l_bat",line_color = "blue")
+		p_prod.line(range(self.horizon),self.data,legend_label="Production solaire",line_color = "red")
+		p_prod.line(range(self.horizon),affLbat-self.data,legend_label="Production vendue",line_color = "yellow")
+
+
+		p_batt = figure(title="Remplissage de la batterie par jour",x_axis_label="pas de temps t",y_axis_label="Énergie stockée (kWh)")
+		p_batt.vbar(x=range(self.horizon),top=affBatt,width=1)
+
+
+		show(row(p_prix,p_prod,p_batt))
+
+
+		return pulp.value(lp.objective)
 
 	def take_decision(self, time):
 
-		stock_remaining = self.battery.getCapacity()-self.battery.getBattery(time-1)
 
-		if time == self.horizon-1 : # au dernier pas de temps, on vend tout le stock
-			self.l_bat[time] = -self.battery.getBattery(time-1)
-		elif self.prices[time]<self.prices[time+1] : # si le prix le prix le lendemain est plus intéressant, on stocke au maximum
-			if self.data[time] < stock_remaining :
-				self.l_bat[time] = self.data[time]
-			else :
-				self.l_bat[time] = stock_remaining
-		else :
-			if self.battery.getBattery(time-1)+self.data[time] < self.pMax : # si on ne dépasse pas la puissance maximale sur la ligne
-				self.l_bat[time] = -self.battery.getBattery(time-1)
-			else :
-				self.l_bat[time]  = self.data[time] - self.pMax
-		self.battery.updateStock(time,self.l_bat[time])
-		return self.l_bat[time]-self.data[time] # on retourne l3(t)
+
+
+
+
+
+		# stock_remaining = self.battery.getCapacity()-self.battery.getBattery(time-1)
+		#
+		# if time == self.horizon-1 : # au dernier pas de temps, on vend tout le stock
+		# 	self.l_bat[time] = -self.battery.getBattery(time-1)
+		# elif self.prices[time]<self.prices[time+1] : # si le prix le prix le lendemain est plus intéressant, on stocke au maximum
+		# 	if self.data[time] < stock_remaining :
+		# 		self.l_bat[time] = self.data[time]
+		# 	else :
+		# 		self.l_bat[time] = stock_remaining
+		# else :
+		# 	if self.battery.getBattery(time-1)+self.data[time] < self.pMax : # si on ne dépasse pas la puissance maximale sur la ligne
+		# 		self.l_bat[time] = -self.battery.getBattery(time-1)
+		# 	else :
+		# 		self.l_bat[time]  = self.data[time] - self.pMax
+		# self.battery.updateStock(time,self.l_bat[time])
+		# print(self.battery.getBattery(time))
+		# return self.l_bat[time]-self.data[time] # on retourne l3(t)
+
+		return 0
+
 
 	def compute_load(self, time):
 		load = self.take_decision(time)
@@ -112,7 +198,10 @@ class Battery :
 		return self.pMax
 
 
-
+player = Player()
+player.set_scenario(data_csv)
+player.set_prices(np.random.rand(48))
+print(player.compute_all_load())
 
 
 
